ebda538751379aa65a0e0376a1eeceaa95ff0504
Jackson Pires <jackson.pires@gmail.com>
2016-10-17 16:37:56 +0000

    First commit

---
+++ app/assets/javascripts/posts.js
@@ -0,0 +1,12 @@
+//$(function(){
+  //$('.post-detail').on('click', function (event) {
+
+  //   event.preventDefault();
+
+  //   var contact = $(this).data('post');
+        
+  //   $('#postModal #post-title').text(contact.title);
+  //   $('#postModal #post-body').text(contact.body);
+  //   $('#postModal').modal('show');
+  //});
+//});
